b62d353f7417ce5fb18a4ef7b7740dc52a39f61d
unknown <ghsherry>
2022-09-03 21:25:05 +0200

    1.0.0

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "header1.h"
+
+int main()
+{
+    int x=10;
+    int y=20;
+    printf("sum of %d & %d=%d \n",x,y, sum_values(x,y));
+    return 0;
+}
---
+++ source.c
@@ -0,0 +1,4 @@
+int sum_values (int a , int b){
+return a+b;
+
+}
